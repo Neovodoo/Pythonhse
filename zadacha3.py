137b86dd23a99badfe3513a2c9319fec65d5014c
Neovodoo <75006634+Neovodoo@users.noreply.github.com>
2020-12-02 12:04:48 +0300

    Create zadacha3.py

---
+++ zadacha3.py
@@ -0,0 +1,15 @@
+x=int(input())
+y=int(input())
+def my_sum(a,b):
+  if (b>=0):
+    while b!=0:
+      a=a+1
+      b=b-1
+      my_sum(a,b)
+  elif (b<0):
+    while b!=0:
+      a=a-1
+      b=b+1
+      my_sum(a,b)
+  return(a)
+print(my_sum(x,y))
